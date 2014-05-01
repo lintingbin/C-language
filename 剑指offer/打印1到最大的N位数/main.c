94c0e37f4a4a5464eaff76c0e06b553f8cb10920
lintingbin2009 <331046161@qq.com>
2014-05-01 21:26:04 +0800

    打印1到最大的N位数

---
+++ 剑指offer/打印1到最大的N位数/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+	//freopen("test.txt", "r", stdin);
+	int count;
+	long long max, num;
+
+	while (scanf("%d", &count) != EOF)
+	{
+		max = num = 1;
+		while (count--)
+		{
+			max *=10;
+		}
+		while (--max)
+			printf("%lld\n", num++);
+
+	}
+	return 0;
+}
+/**************************************************************
+Problem: 1515
+User: lintingbin2009
+Language: C
+Result: Accepted
+Time:40 ms
+Memory:912 kb
+ ****************************************************************/
