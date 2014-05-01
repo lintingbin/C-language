606b9b5ed039c49fc5097280feaf12698b3f9d9d
lintingbin2009 <331046161@qq.com>
2014-05-01 12:50:03 +0800

    update

---
+++ 剑指offer/从头到尾打印链表/main.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+struct node {
+	int data;
+	struct node *next;
+};
+
+int main()
+{
+	//freopen("test.txt", "r", stdin);
+	int input;
+	struct node *head, *cur, *tmp;
+
+	head = (struct node *)malloc(sizeof(struct node));
+	cur = head;
+	scanf("%d", &input);
+	while (input != -1)
+	{
+		tmp = (struct node *)malloc(sizeof(struct node));
+		tmp->data = input;
+		tmp->next = NULL;
+		cur->next = tmp;
+		cur = cur->next;
+		scanf("%d", &input);
+	}
+	cur = head->next;
+	head->next = NULL;
+	while (cur)
+	{
+		tmp = head->next;
+		head->next = cur;
+		cur = cur->next;
+		head->next->next = tmp;
+	}
+	cur = head->next;
+	while (cur)
+	{
+		printf("%d\n", cur->data);
+		cur = cur->next;
+	}
+	return 0;
+}
+
+/**************************************************************
+Problem: 1511
+User: lintingbin2009
+Language: C
+Result: Accepted
+Time:90 ms
+Memory:3948 kb
+****************************************************************/
